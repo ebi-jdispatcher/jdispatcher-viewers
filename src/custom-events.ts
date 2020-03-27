9c53154893a3f33adaf1e619a2c448b15d3a3125
Fábio Madeira <fbiomadeira@gmail.com>
2020-03-27 17:30:27 +0000

    JD-579 Added new custom events for selecting the scale color scheme checkbox

--- src/custom-events.ts
+++ src/custom-events.ts
@@ -1,5 +1,6 @@
 import { fabric } from "fabric";
 import { TextType } from "./custom-types";
+import { FabricjsRenderer } from "./app";
 
 export function mouseOverText(
     fabricObj: fabric.Object,
@@ -67,6 +68,55 @@ export function mouseOutDomain(
         if (e.target) {
             fabricGroupObj.set({ visible: false });
             canvas.renderAll();
+        }
+    });
+}
+
+export function mouseOverCheckbox(
+    fabricObj: fabric.Object,
+    textObj: TextType,
+    _this: FabricjsRenderer
+) {
+    fabricObj.on("mouseover", (e: fabric.IEvent) => {
+        if (e.target) {
+            e.target.set("hoverCursor", "default");
+            e.target.setOptions(textObj);
+            e.target.setOptions({ fill: "black" });
+            _this.canvas.renderAll();
+        }
+    });
+}
+
+export function mouseDownCheckbox(
+    fabricObj: fabric.Object,
+    value: string,
+    _this: FabricjsRenderer
+) {
+    fabricObj.on("mousedown", (e: fabric.IEvent) => {
+        if (e.target) {
+            if (_this.scaleType != value) {
+                _this.scaleType = value;
+                _this.renderAll();
+            }
+        }
+    });
+}
+
+export function mouseOutCheckbox(
+    fabricObj: fabric.Object,
+    textObj: TextType,
+    value: string,
+    _this: FabricjsRenderer
+) {
+    fabricObj.on("mouseout", (e: fabric.IEvent) => {
+        if (e.target) {
+            e.target.setOptions(textObj);
+            if (_this.scaleType != value) {
+                e.target.setOptions({
+                    fill: "grey"
+                });
+            }
+            _this.canvas.renderAll();
         }
     });
 }
