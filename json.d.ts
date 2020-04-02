6548cd5d6bf9cb8a5892af3d26bc76e701f41972
Fábio Madeira <fbiomadeira@gmail.com>
2020-04-02 12:27:28 +0100

    JD-586 Updated ts config and autoformatted the json.d.ts

--- json.d.ts
+++ json.d.ts
@@ -1,11 +1,12 @@
 // This will allow you to load `.json` files from disk
 declare module "*.json" {
-  const value: any;
-  export default value;
+    const value: any;
+    export default value;
 }
 
 // This will allow you to load JSON from remote URL responses
 declare module "json!*" {
-  const value: any;
-  export default value;
+    const value: any;
+    export default value;
+}
 }
