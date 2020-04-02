e027d65bbb7b491f668d98eec2bf50d5be49bcfd
Fábio Madeira <fbiomadeira@gmail.com>
2020-04-02 12:45:21 +0100

    JD-586 Updated webpack config for prod to use version instead of hash

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const cleanPlugin = require("clean-webpack-plugin");
+const packageJson = require(path.join(process.cwd(), "package.json"));
 
 module.exports = {
     mode: "production",
     entry: "./src/app.ts",
     output: {
-        filename: "jd_viewers_bundle.[hash].js",
+        // filename: "jd_viewers_bundle_[hash].js",
+        filename: `jd_viewers_bundle_${packageJson.version}.js`,
         path: path.resolve(__dirname, "dist")
     },
     devtool: "none",
