7f8829e15757c734ab2f454d3564fd1e0fb56d3e
Fábio Madeira <fbiomadeira@gmail.com>
2020-04-14 16:44:52 +0100

    JD-592 Added new app.ts

---
+++ src/app.ts
@@ -0,0 +1,5 @@
+import { VisualOutput } from "./visual-output-app";
+import { FunctionalPredictions } from "./functional-predictions-app";
+
+(window as any).VisualOutput = VisualOutput;
+(window as any).FunctionalPredictions = FunctionalPredictions;
