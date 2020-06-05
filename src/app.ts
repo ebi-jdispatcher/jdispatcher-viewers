40ded24692d273d44bbb72bfa19a8c59ba43b865
Fábio Madeira <fbiomadeira@gmail.com>
2020-06-05 18:04:55 +0100

    JD-631 Exporting all the required classes, functions and consts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import svgToMiniDataURI from "mini-svg-data-uri";
+import { isBrowser, isNode } from "browser-or-node";
 import { VisualOutput } from "./visual-output-app";
 import { FunctionalPredictions } from "./functional-predictions-app";
 import {
@@ -10,22 +11,42 @@ import {
     validateSubmittedDbfetchInput,
     getIPRMCDataModelFlatFromXML,
 } from "./other-utilities";
+import { ColorSchemeEnum } from "./custom-types";
 
-// plugin support
-(window as any).VisualOutput = VisualOutput;
-(window as any).FunctionalPredictions = FunctionalPredictions;
-(window as any).validateJobId = validateJobId;
-(window as any).svgToMiniDataURI = svgToMiniDataURI;
-(window as any).fetchData = fetchData;
-(window as any).dataAsType = dataAsType;
-(window as any).validateSubmittedJobIdInput = validateSubmittedJobIdInput;
-(window as any).validateSubmittedDbfetchInput = validateSubmittedDbfetchInput;
-(window as any).getIPRMCDataModelFlatFromXML = getIPRMCDataModelFlatFromXML;
-(window as any).getJdispatcherJsonURL = getJdispatcherJsonURL;
+// plugin support & module support
+// if (typeof window === "undefined") {
+if (isBrowser) {
+    (window as any).VisualOutput = VisualOutput;
+    (window as any).FunctionalPredictions = FunctionalPredictions;
+    (window as any).validateJobId = validateJobId;
+    (window as any).svgToMiniDataURI = svgToMiniDataURI;
+    (window as any).fetchData = fetchData;
+    (window as any).dataAsType = dataAsType;
+    (window as any).getJdispatcherJsonURL = getJdispatcherJsonURL;
+    (window as any).validateSubmittedJobIdInput = validateSubmittedJobIdInput;
+    (window as any).validateSubmittedDbfetchInput = validateSubmittedDbfetchInput;
+    (window as any).getIPRMCDataModelFlatFromXML = getIPRMCDataModelFlatFromXML;
+    (window as any).ColorSchemeEnum = ColorSchemeEnum;
+}
+if (isNode) {
+    module.exports = {
+        VisualOutput: VisualOutput,
+        FunctionalPredictions: FunctionalPredictions,
+        validateJobId: validateJobId,
+        svgToMiniDataURI: svgToMiniDataURI,
+        fetchData: fetchData,
+        dataAsType: dataAsType,
+        getJdispatcherJsonURL: getJdispatcherJsonURL,
+        validateSubmittedJobIdInput: validateSubmittedJobIdInput,
+        validateSubmittedDbfetchInput: validateSubmittedDbfetchInput,
+        getIPRMCDataModelFlatFromXML: getIPRMCDataModelFlatFromXML,
+        ColorSchemeEnum: ColorSchemeEnum,
+    };
+}
 
 // web-component support
 import "./visual-output-webcomponent.ts";
 import "./functional-predictions-webcomponent.ts";
 
-// demo index page with JobId input form
+// demo index page with JobId input form (implemented in TypeScript)
 import "./index.ts";
