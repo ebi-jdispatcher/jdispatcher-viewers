eaa71fa0849b0e5e19107e3001c565288749e539
Fábio Madeira <fabiomadeira@me.com>
2022-08-11 09:10:06 +0100

    build: adding index.html to src and udating the webpack config accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -35,11 +35,11 @@ export default {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./static/index.html"
+            template: "./src/index.html"
         }),
         new CopyPlugin({
             patterns: [
-                { from: path.join(__dirname, 'static', "style.css"),
+                { from: path.join(__dirname, 'docs', "style.css"),
                     to: path.join(__dirname, 'dist', "style.css")},
                 { from: path.join(__dirname, 'src', "testdata"),
                     to: path.join(__dirname, 'dist', "testdata")},
