56deaccbfff728ac0e058ed31819b047ca8b7e3c
Fábio Madeira <fabiomadeira@me.com>
2024-10-30 14:57:21 +0000

    auto-formatting

--- webpack.config.cli.js
+++ webpack.config.cli.js
@@ -1,40 +1,40 @@
 import path from 'node:path';
-import {fileURLToPath} from 'node:url';
-import cleanPlugin from "clean-webpack-plugin";
-import nodeExternals from "webpack-node-externals";
+import { fileURLToPath } from 'node:url';
+import cleanPlugin from 'clean-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-    mode: "production",
-    entry: "./src/jd-viewers-cli.ts",
-    output: {
-        filename: `jd-viewers-cli.js`,
-        path: path.resolve(__dirname, "bin"),
-        library: {
-            type: "module",
+  mode: 'production',
+  entry: './src/jd-viewers-cli.ts',
+  output: {
+    filename: `jd-viewers-cli.js`,
+    path: path.resolve(__dirname, 'bin'),
+    library: {
+      type: 'module',
+    },
+    libraryTarget: 'module',
+  },
+  experiments: {
+    outputModule: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts)$/,
+        loader: 'ts-loader',
+        exclude: [/node_modules/],
+        options: {
+          configFile: 'tsconfig.cli.json',
         },
-        libraryTarget: 'module',
-    },
-    experiments: {
-        outputModule: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts)$/,
-                loader: "ts-loader",
-                exclude: [/node_modules/],
-                options: {
-                    configFile: "tsconfig.cli.json",
-                },
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    plugins: [new cleanPlugin.CleanWebpackPlugin()],
-    node: false,
-    externals: [nodeExternals()],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  // plugins: [new cleanPlugin.CleanWebpackPlugin()],
+  node: false,
+  externals: [nodeExternals()],
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,47 @@
 import path from 'node:path';
-import {fileURLToPath} from 'node:url';
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import CopyPlugin from "copy-webpack-plugin";
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
-    mode: "development",
-    entry: "./src/app.ts",
-    output: {
-        filename: "jd_viewers.bundle.js",
-        path: path.resolve(__dirname, "dist")
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'jd_viewers.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+    compress: false,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+      watch: true,
     },
-    devtool: "inline-source-map",
-    devServer: {
-        port: 8080,
-        compress: false,
-        static: {
-            directory: path.join(__dirname, 'dist'),
-            watch: true
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js", ".json"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./static/index.html"
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: path.join(__dirname, 'static', "style.css"),
-                    to: path.join(__dirname, 'dist', "style.css")},
-                { from: path.join(__dirname, 'src', "testdata"),
-                    to: path.join(__dirname, 'dist', "testdata")},
-            ],
-        })
-    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './static/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.join(__dirname, 'static', 'style.css'), to: path.join(__dirname, 'dist', 'style.css') },
+        { from: path.join(__dirname, 'src', 'testdata'), to: path.join(__dirname, 'dist', 'testdata') },
+      ],
+    }),
+  ],
 };
