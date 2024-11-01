8b86ef31b4c3e4c9bb59f276f8e15fcd6236b6de
Fábio Madeira <fabiomadeira@me.com>
2024-11-01 11:31:50 +0000

    test: adds basic config and setup for running jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+  },
+};
