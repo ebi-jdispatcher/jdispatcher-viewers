ea6f64215161d4737b68323ad5f5e6a188a1014b
Fábio Madeira <fabiomadeira@me.com>
2025-03-03 17:53:06 +0000

    adds typedoc config and an helper script to fix the 'go home' URL

---
+++ docs/fix-home-typedoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DOCS_DIR="docs/api"
+NEW_URL="/jdispatcher-viewers/index.html"
+
+# Find all .html files recursively
+find "$DOCS_DIR" -type f -name "*.html" | while read -r FILE; do
+  if [ -f "$FILE" ]; then
+    sed -i "s|<a href=\"index.html\"|<a href=\"$NEW_URL\"|" "$FILE"
+    sed -i "s|<a href=\"../index.html\"|<a href=\"$NEW_URL\"|" "$FILE"
+    echo "Updated $FILE"
+  else
+    echo "File not found: $FILE"
+  fi
+done
