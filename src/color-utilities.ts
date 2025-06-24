d03bfd525083b6a49534fa0f0c7138e005394734
Fábio Madeira <fabiomadeira@me.com>
2025-06-24 13:06:47 +0100

    adds prettier and fixes format problems

--- src/color-utilities.ts
+++ src/color-utilities.ts
@@ -367,22 +367,22 @@ export function HSVtoRGB(h: number, s: number, v: number) {
   t = v * (1 - (1 - f) * s);
   switch (i % 6) {
     case 0:
-      (r = v), (g = t), (b = p);
+      ((r = v), (g = t), (b = p));
       break;
     case 1:
-      (r = q), (g = v), (b = p);
+      ((r = q), (g = v), (b = p));
       break;
     case 2:
-      (r = p), (g = v), (b = t);
+      ((r = p), (g = v), (b = t));
       break;
     case 3:
-      (r = p), (g = q), (b = v);
+      ((r = p), (g = q), (b = v));
       break;
     case 4:
-      (r = t), (g = p), (b = v);
+      ((r = t), (g = p), (b = v));
       break;
     case 5:
-      (r = v), (g = p), (b = q);
+      ((r = v), (g = p), (b = q));
       break;
   }
   return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
